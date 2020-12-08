1b22157003888c9d4b0c2d8b72128493ac50c93e
vight <highvight@tutamail.com>
2020-12-08 11:00:55 +0100

    Update setup reuqirements to use tcp_latency instead of salvo

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_lb',
       version='0.0.1',
-      install_requires=['docker','gnlpy','gym','salvo']
+      install_requires=['docker','gnlpy','gym','tcp_latency']
 )
